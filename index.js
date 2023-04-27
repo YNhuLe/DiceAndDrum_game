4a2bca5935613956a657fa33616469bf9d4e336e
NhuYLe <lenhuy@sheridancollege.ca>
2023-04-27 10:54:10 -0400

    modify switch case

--- index.js
+++ index.js
@@ -12,32 +12,33 @@ for (i = 0; i < numOfDrumButtons; i++) {
       case "w":
         var crash = new Audio("sounds/crash.mp3");
         crash.play();
-        this.style.color = "blue";
-
+        break;
       case "a":
         var kick = new Audio("sounds/kick-bass.mp3");
         kick.play();
-        this.style.color = "green";
+        break;
       case "s":
         var snare = new Audio("sounds/snare.mp3");
         snare.play();
-        this.style.color = "red";
+        break;
       case "d":
         var tom1 = new Audio("sounds/tom-1.mp3");
         tom1.play();
-        this.style.color = "orange";
+        break;
       case "j":
         var tom2 = new Audio("sounds/tom-2.mp3");
         tom2.play();
-        this.style.color = "purple";
+        break;
       case "k":
         var tom3 = new Audio("sounds/tom-3.mp3");
         tom3.play();
-        this.style.color = "yellow";
+        break;
       case "l":
         var tom4 = new Audio("sounds/tom-4.mp3");
         tom4.play();
-        this.style.color = "pink";
+        break;
+      default:
+        console.log(buttonHTML);
     }
   });
 }
